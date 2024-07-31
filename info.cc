689b562a40f5647dc1f4a308944e8904d1017fae
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-07-31 09:13:06 +0300

    расширен метод TaskList::info

--- info.cc
+++ info.cc
@@ -2,13 +2,16 @@
 #include <iostream>
 
 namespace tl {
-   void TaskList::info()
-      // функция выводит логотип, справку и приглашение к работе
-   {
-      std::cout << "Task list. Version 1.0.0\n"
-                << "(c) 2024 S-Patriarch\n"
-                << "\nCommands:\n"
-                << "  q - exit\n"
-                << "\n:";
-   }
+  void TaskList::info()
+    // функция выводит логотип, справку и приглашение к работе
+  {
+    std::cout << "Task list. Version 1.0.0\n"
+              << "(c) 2024 S-Patriarch\n"
+              << "\nCommands:\n"
+              << "  a - add entry\n"
+              << "  d - delete entry\n"
+              << "  e - edit entry\n"
+              << "  q - exit\n"
+              << "\n:";
+  }
 }
