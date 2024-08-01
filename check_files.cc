3a3fb6f6fb9f415fd1d31c281c3a85f52efbcc3a
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-08-01 16:25:05 +0300

    чистка кода

--- check_files.cc
+++ check_files.cc
@@ -6,16 +6,16 @@
 namespace tl {
   bool TaskList::check_files() const
   {
-    if (m_filePath=="") {
+    if (m_filePath == "") {
       return false;
     } else {
       const char* dirPath = m_filePath.c_str();
-      mkdir(dirPath,0700);
+      mkdir(dirPath, 0700);
 
-      std::array<std::string,2> arr {m_fileData,m_fileConfig};
+      std::array<std::string, 2> arr {m_fileData, m_fileConfig};
       std::fstream fs;
       for (const auto& file : arr) {
-        fs.open(file,std::ios::in|std::ios::out|std::ios::app);
+        fs.open(file, std::ios::in|std::ios::out|std::ios::app);
         if (!fs.is_open()) return false;
         else fs.close();
       }
